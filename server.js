fac7d9e955314c21ac087090ad8ce04663851346
Banh <tommyngo156@gmail.com>
2026-07-08 02:20:04 -0400

    feat: Add caching using Redis

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const { Pool } = require('pg');
-const dotenv = require('dotenv');
+const express = require("express");
+const { Pool } = require("pg");
+const  redis  = require("redis");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
@@ -10,20 +11,53 @@ const pool = new Pool({
   connectionString: process.env.PG_CONNECTION_STRING,
 });
 
-app.get('/players', async (req, res) => {
+const redisClient = redis.createClient({url: process.env.REDIS_URL});
+
+redisClient.on("error", (err) => {
+  console.error("Redis error:", err);
+});
+
+redisClient.on("connect", () => {
+  console.log("Connected to Redis");
+});
+
+redisClient.connect().catch((err) => {
+  console.error("Error connecting to Redis:", err);
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the World Cup Data API");
+});
+
+app.get("/players", async (req, res) => {
   try {
-    const playerResult = await pool.query('SELECT * FROM player');
-    const goalkeeperResult = await pool.query('SELECT * FROM goalkeepers');
-    res.json({
-      players: playerResult.rows,
-      goalkeepers: goalkeeperResult.rows,
+    const cachedPlayers = await redisClient.get("players");
+    if (cachedPlayers) {
+      console.log("Returning cached players");
+      return res.json(JSON.parse(cachedPlayers));
+    }
+    const playerResult = await pool.query("SELECT * FROM player");
+    const goalkeeperResult = await pool.query("SELECT * FROM goalkeepers");
+    const players = playerResult.rows;
+    const goalkeepers = goalkeeperResult.rows;
+
+    await redisClient.set("players", JSON.stringify({ players, goalkeepers }), {
+      EX: 3600,
     });
+    await redisClient.set("goalkeepers", JSON.stringify(goalkeepers), {
+      EX: 3600,
+    });
+    res.json({
+      players: players,
+      goalkeepers: goalkeepers,
+    });
+
   } catch (error) {
-    console.error('Error fetching players:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error fetching players:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
