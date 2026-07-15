422c92db7345b311f6a8e3fd330a33694100a779
Banh <tommyngo156@gmail.com>
2026-07-14 21:19:44 -0400

    fix: Wrap Redis in their own try/catch block

--- app.js
+++ app.js
@@ -14,22 +14,31 @@ app.use(express.static(path.join(__dirname, "views")));
 
 app.get("/players", rateLimiter, async (req, res) => {
   try {
-    const cachedPlayers = await redisClient.get("players");
-    if (cachedPlayers) {
-      console.log("Returning cached players");
-      return res.json(JSON.parse(cachedPlayers));
+    try {
+      const cachedPlayers = await redisClient.get("players");
+      if (cachedPlayers) {
+        console.log("Returning cached players");
+        return res.json(JSON.parse(cachedPlayers));
+      }
+    } catch (error) {
+      console.error("Error fetching from Redis:", error);
     }
+
     const playerResult = await pool.query("SELECT * FROM player");
     const goalkeeperResult = await pool.query("SELECT * FROM goalkeepers");
     const players = playerResult.rows;
     const goalkeepers = goalkeeperResult.rows;
     const payload = { players, goalkeepers };
 
-    await redisClient.set("players", JSON.stringify(payload), {
-      EX: 3600,
-    });
-    res.json(payload);
+    try {
+      await redisClient.set("players", JSON.stringify(payload), {
+        EX: 3600,
+      });
+    } catch (error) {
+      console.error("Error caching players in Redis:", error);
+    }
 
+    res.json(payload);
   } catch (error) {
     console.error("Error fetching players:", error);
     res.status(500).json({ error: "Internal Server Error" });
